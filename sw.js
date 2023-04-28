06c28fe04b91d67c6d1381494eea2c5f307d7727
有田海斗（Arita Kaito） <130941882+Arita-Kaito@users.noreply.github.com>
2023-04-28 14:10:12 +0900

    PWA対応．

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Cache name
+const CACHE_NAME = 'pwa-caches';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './indexLogin.html',
+  './pages/b.html',
+  './style.css',
+  './styleLogin.css',
+  './lib/sweetalert2.js',
+  './lib/sweetalert2Dark.css',
+  './img/backgroundBlack.jpg',
+  './img/backgroundGreen.jpg',
+  './img/backgroundPink.jpg',
+  './img/OnlineForm.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
